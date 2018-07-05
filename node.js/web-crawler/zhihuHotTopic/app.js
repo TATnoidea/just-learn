bbb2f41d3056d10b3fe54abe2385ffb9dc7f5ab3
chengzichao <chengzichao@lansheng8.com>
2018-07-05 17:12:43 +0800

    优化读取文件

--- node.js/web-crawler/zhihuHotTopic/app.js
+++ node.js/web-crawler/zhihuHotTopic/app.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
-const path = require("path");
-const request = require("request");
-const cheerio = require("cheerio");
-const moment = require("moment");
+const fs = require("fs"); // fs模块
+const path = require("path"); // path模块
+const request = require("request"); // 通过request发送请求
+const cheerio = require("cheerio"); // 通过cheerio来获取DOM
+const moment = require("moment"); // 格式化时间
 
-const baseUrl = "https://www.zhihu.com";
+const baseUrl = "https://www.zhihu.com"; // 基本链接
 
 function _main() {
   request(`${baseUrl}/explore`, (error, response, body) => {
@@ -25,8 +25,8 @@ function _main() {
     const monthlyRecord = new RECORD(time, monthlyArr);
 
     // 保存文件路径
-    const dailyFileUrl = path.resolve(__dirname, "./dailyTopic.json");
-    const monthlyFileUrl = path.resolve(__dirname, "./monthlyTopic.json");
+    const dailyFileUrl = path.resolve(__dirname, "./daily_topic.json");
+    const monthlyFileUrl = path.resolve(__dirname, "./monthly_topic.json");
     writeFile(dailyFileUrl, dailyRecord);
     writeFile(monthlyFileUrl, monthlyRecord);
   });
@@ -41,14 +41,11 @@ function RECORD(time, topics) {
 function getTopicInfo(ele, index, time) {
   const topic = {};
   topic.rank = index + 1; // 排名
-  topic.title = ele
-    .find("h2")
-    .text()
-    .trim(); // 标题
+  topic.title = ele.find("h2").text().trim(); // 标题
   topic.author = ele.find(".author-link").text(); // 作者
   topic.agree = ele.find(".count").text(); // 赞数
   let summary = ele.find(".summary").text(); // 简介
-  topic.summary = summary.substring(0, summary.length - 8).trim();
+  topic.summary = summary.substring(0, summary.length - 8).trim(); // 因为简介后面有显示全文所以需要删除掉
   topic.url = baseUrl + ele.find(".question_link").attr("href"); // 链接
   return topic;
 }
@@ -63,29 +60,33 @@ function getTopics(ele) {
   return arr;
 }
 
+
 // 写入文件
 function writeFile(url, data) {
-  const fileUrl = url;
-  const appendData = data;
-  if (fileExist(fileUrl)) {
+  const fileUrl = url;  // 文件地址
+  const appendData = data;  // 需要添加的数据
+  // 判断文件是否存在
+  if (fs.existsSync(fileUrl)) {
+    // 文件存在
     fs.readFile(fileUrl, "utf-8", (err, data) => {
-      let oldData = JSON.parse(data);
-      oldData.push(appendData);
-      const newData = JSON.stringify(oldData);
+      const oldData = JSON.parse(data); // 旧的数据
+      oldData.push(appendData); // 将新添加的数据push进旧的数据中
+      const newData = JSON.stringify(oldData);  // 将对象字符串化
+      // 写入文件
       fs.writeFile(fileUrl, newData, err => {
         if (err) throw err;
       });
     });
   } else {
+    // 文件不存在
+    const data = JSON.stringify([appendData]);
+    // writeFile中的flag设为w+时，不存在文件会创建文件
+    fs.writeFile(fileUrl, data, {flag: 'w+'}, err => {
+      if (err) throw err;
+    });
   }
 }
 
-// 判断文件是否存在
-function fileExist(url) {
-  fs.access(url, fs.constants.F_OK, err => {
-    return false;
-  });
-  return true;
-}
+
 
 _main();
