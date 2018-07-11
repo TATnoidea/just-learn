89bb724a9e105513e456d7db9c010e6b5b12ca16
chengzichao <chengzichao@lansheng8.com>
2018-07-11 11:01:42 +0800

    koa-bodyparser

--- node.js/koa/koa-router/src/app.js
+++ node.js/koa/koa-router/src/app.js
@@ -1,6 +1,6 @@
 const Koa = require('koa');
-
 const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 
@@ -15,8 +15,29 @@ router.get('/hello/:name', async (ctx, next) => {
 });
 
 router.get('/', async (ctx, next) => {
-  ctx.response.body = `<h1>Index</h1>`;
+  ctx.response.body = `<h1>Index</h1>
+  <form action='/signin' method='post'>
+    <p>Name: <input name='name' type='text'></p>
+    <p>Password:<input name='password' type='password'></p>
+    <p><input type='submit' value='submit'></p>
+  </form>`;
 });
+
+router.post('/signin', async (ctx, next) => {
+  const name = ctx.request.body.name || '';
+  const password = ctx.request.body.password || '';
+  console.log(`signin with name: ${ name }, password: ${ password }`);
+
+  if(name === 'charles' && password === '123') {
+    ctx.response.body = `<h1>Welcome, ${ name }</h1>`;
+  } else {
+    ctx.response.body = `<h1>Login failed!</h1>
+    <p><a herf='/'>Try again</a></p>`;
+    
+  }
+})
+
+app.use(bodyParser());
 app.use(router.routes());
 
 app.listen(3000, () => {
