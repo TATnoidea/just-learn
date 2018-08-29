6e41d1758c2774a67dd218541eb8cc36bd03dc1a
chengzichao <592292495@qq.com>
2018-08-29 18:34:54 +0800

    use pipes

--- front-end/angular/pipes/src/app/app.module.ts
+++ front-end/angular/pipes/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HeroBirthdayComponent } from './hero-birthday1.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroBirthdayComponent,
   ],
   imports: [
     BrowserModule
