52527c4334e29b79ce66ddbc7ecc1706fb8877a9
chengzichao <592292495@qq.com>
2019-02-16 14:24:04 +0800

    change directory

--- front-end/angular/component-styles/src/app/quest-summary.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'app-quest-summary',
-  templateUrl: './quest-summary.component.html',
-  styleUrls: ['./quest-summary.component.css']
-})
-export class QuestSummaryComponent { }
